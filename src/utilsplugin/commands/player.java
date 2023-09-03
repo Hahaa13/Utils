6603a0748de1de31dc1dc04d8d4d4b5381530a9f
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-03 16:46:02 +0700

    Create player.java

---
+++ src/utilsplugin/commands/player.java
@@ -0,0 +1,13 @@
+package utilsplugin.commands;
+
+import arc.*;
+import mindustry.*;
+import mindustry.core.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+
+public class player {
+  public static void load() {
+    register("discord", (args, player) -> Call.openURI(player.con, ""));
+  }
+}
