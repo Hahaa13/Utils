8a8dbb8e6e1449063e3c1d2075972360017f70fa
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-03 17:13:37 +0700

    Update and rename player.java to playercommands.java

--- src/utilsplugin/commands/playercommands.java
+++ src/utilsplugin/commands/playercommands.java
@@ -7,7 +7,7 @@ import mindustry.core.*;
 import mindustry.game.*;
 import mindustry.gen.*;
 
-public class player {
+public class playercommands {
   public static void load(CommandHandler handler) {
     handler.<Player>register("discord", "Discord server link", (args, player) -> Call.openURI(player.con, "https://discord.gg/Dxk9PxuDq4"));
   }
