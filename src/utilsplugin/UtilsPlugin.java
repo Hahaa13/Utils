eee3d83737d2313828a02be0262100a674a7b3fc
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-03 17:15:19 +0700

    Update UtilsPlugin.java

--- src/utilsplugin/UtilsPlugin.java
+++ src/utilsplugin/UtilsPlugin.java
@@ -1,6 +1,7 @@
 package utilsplugin;
 
 import utilsplugin.events.*;
+import utilsplugin.commands.*;
 import mindustry.*;
 import mindustry.mod.*;
 
@@ -8,5 +9,9 @@ public class UtilsPlugin extends Plugin {
   @Override
   public void init() {
     event.load();
+  }
+  @Override
+  public void registerClientCommands(CommandHandler handler){
+    playercommands.load(hanlder);
   }
 }
