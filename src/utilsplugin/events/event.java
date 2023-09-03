6927d4ee8a2aa8199f4f1e9d05fdd92f94fb96ab
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-03 15:15:26 +0700

    Create event.java

---
+++ src/utilsplugin/events/event.java
@@ -0,0 +1,22 @@
+package utilsplugin.events;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.core.*;
+import mindustry.gen.*;
+import mindustry.game.*;
+
+public class event {
+  public static void load() {
+    Events.on(ServerLoadEvent.class, e -> {
+      Log.info("UtilsPlugin Loading");
+    });
+    Events.on(PlayerJoin.class, e-> {
+      Player p = e.player;
+      if (p.admin()) {
+        p.name() = "[red][A]" + p.name()
+      }
+    });
+  }
+}
