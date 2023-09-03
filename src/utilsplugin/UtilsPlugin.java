5da220be9bf29bcc78d43bad2a6fdf7363b62cd7
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-03 14:56:44 +0700

    Update UtilsPlugin.java

--- src/utilsplugin/UtilsPlugin.java
+++ src/utilsplugin/UtilsPlugin.java
@@ -1,8 +1,12 @@
 package utilsplugin;
 
+import utilsplugin.events.*;
 import mindustry.*;
 import mindustry.mod.*;
 
 public class UtilsPlugin extends Plugin {
   @override
+  public void init() {
+    event.load()
+  }
 }
