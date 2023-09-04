303f6c61825346d5943600ca1b1ee606fded25b2
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-04 18:09:12 +0700

    Update config.java

--- src/utilsplugin/utils/config.java
+++ src/utilsplugin/utils/config.java
@@ -1,8 +1,15 @@
 package utilsplugin.utils;
 
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 import org.json.simple.JSONObject;
 
-public class config {
+public class config(String key, String value) {
+  public static String add() {
+    JSONObject json = new JSONObject();
+    json.put(key, value);
+  }
+  public static String get() {
+  }
 }
