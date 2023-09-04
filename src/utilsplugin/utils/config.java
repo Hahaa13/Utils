562ded3a2c0ac20d6476cfb151b6bc222d0c8e84
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-04 18:34:58 +0700

    Update config.java

--- src/utilsplugin/utils/config.java
+++ src/utilsplugin/utils/config.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import org.json.simple.JSONObject;
 
-public class config() {
+public class config {
   public static String add(String key, String value) {
     JSONObject json = new JSONObject();
     json.put(key, value);
