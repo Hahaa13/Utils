fad1af2e683d7f9351dacfa637d6a0eb71157a14
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-04 18:55:18 +0700

    Update UtilsPlugin.java

--- src/utilsplugin/UtilsPlugin.java
+++ src/utilsplugin/UtilsPlugin.java
@@ -2,6 +2,7 @@ package utilsplugin;
 
 import utilsplugin.events.*;
 import utilsplugin.commands.*;
+import utilsplugin.utils.config;
 import arc.util.*;
 import mindustry.*;
 import mindustry.mod.*;
@@ -10,6 +11,7 @@ public class UtilsPlugin extends Plugin {
   @Override
   public void init() {
     event.load();
+    config.add("token", "token");
   }
   @Override
   public void registerClientCommands(CommandHandler handler){
