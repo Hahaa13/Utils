64bd45d020c24d4305cd4a7e9e805f963cf781d8
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-05 16:38:54 +0700

    Create findplayer.java

---
+++ src/utilsplugin/utils/findplayer.java
@@ -0,0 +1,20 @@
+package utilsplugin.utils;
+
+import arc.util.Nullable;
+import arc.util.Strings;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
+
+public class Players {
+    public static @Nullable Player getPlayer(String name) {
+        return Groups.player.find(p -> Strings.stripColors(p.name).equalsIgnoreCase(name));
+    }
+
+    public static @Nullable Player getPlayerbyUUID(String UUID) {
+        return Groups.player.find(p -> p.uuid().equals(UUID));
+    }
+
+    public static @Nullable Player getPlayerbyID(int id) {
+        return Groups.player.find(p -> p.id == id);
+    }
+}
