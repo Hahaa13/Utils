b08e68f8358c36c3a3dfbf713d3bdbf4d9003df8
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-05 18:53:44 +0700

    Update event.java

--- src/utilsplugin/events/event.java
+++ src/utilsplugin/events/event.java
@@ -10,9 +10,6 @@ import mindustry.game.EventType.*;
 
 public class event {
   public static void load() {
-    Events.on(ServerLoadEvent.class, e -> {
-      Log.info("UtilsPlugin Loading");
-    });
     Events.on(PlayerJoin.class, e-> {
       Player p = e.player;
       if (p.admin) {
