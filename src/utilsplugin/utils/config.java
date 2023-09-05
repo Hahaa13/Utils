1117945115b55f39f677c7172fa15c42311ea5ba
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-05 17:18:33 +0700

    Update config.java

--- src/utilsplugin/utils/config.java
+++ src/utilsplugin/utils/config.java
@@ -2,11 +2,29 @@ package utilsplugin.utils;
 
 import arc.util.Log;
 import java.io.FileWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import org.json.JSONObject;
 
 public class config {
+  private String fileconfig = "config/mods/UtilsPlugin/config.json"
   public static void add(String key, String value) {
+    JSONObject json = new JSONObject();
+    File dir = new File("/config/mods/UtilsPlugin";
+    if (!dir.exists()) dir.mkdirs();
+    try {
+      FileReader file = FileReader(fileconfig);
+      json = new JSONObject(file);
+    }
+    json.put(key, value);
+    try {
+      FileWriter file = new FileWriter(fileconfig);
+      file.write(json.toString());
+      file.close();
+    } catch (IOException e) {
+      Log.error("UtilsPlugin cannot write file config");
+    }
   }
   public static void get(String key) {
   }
