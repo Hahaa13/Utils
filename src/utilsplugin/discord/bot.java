3f4e0283b2a4a1523f4743ccbd25358d8c0c55c6
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-07 08:28:14 +0700

    Update bot.java

--- src/utilsplugin/discord/bot.java
+++ src/utilsplugin/discord/bot.java
@@ -4,6 +4,7 @@ import utilsplugin.utils.config;
 import discord4j.core.*;
 import discord4j.gateway.intent.*;
 import discord4j.common.util.Snowflake;
+import discord4j.core.object.entity.Member;
 
 public class bot {
   public static void load() {
@@ -18,7 +19,9 @@ public class bot {
       var member = e.getMember().orElse(null);
       if (member == null || member.isBot()) return;
 
-      if(message.getChannelId() == Snowflake.of(config.get("discordbot_channelID"))) {
+      if(message.getChannelId() == Snowflake.of(config.get("discordbot_channelid"))) {
+        String content = message.getContent();
+        String user = member.getNickname();
       }
     }
   }
