f33df0b97ad7d3e23d0c8036ada5a8fc8c01b9a1
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-07 09:51:05 +0700

    Update bot.java

--- src/utilsplugin/discord/bot.java
+++ src/utilsplugin/discord/bot.java
@@ -8,21 +8,24 @@ import discord4j.gateway.intent.*;
 import discord4j.common.util.Snowflake;
 import discord4j.core.object.entity.Member;
 import discord4j.core.event.domain.message.*;
+import discord4j.core.object.entity.channel.GuildMessageChannel;
 
 public class bot {
+  public static GuildMessageChannel channel;
   public static void load() {
     GatewayDiscordClient gateway = DiscordClient.create(config.get("discordbot_token"))
                                                         .gateway()
                                                         .setEnabledIntents(IntentSet.of(Intent.GUILD_MEMBERS, Intent.GUILD_MESSAGES))
                                                         .login()
                                                         .block();
+    channel = gateway.getChannelById(Snowflake.of(config.get("discordbot_channelid"))).ofType(GuildMessageChannel.class).block();
     gateway.on(MessageCreateEvent.class).subscribe(e -> {
       var message = e.getMessage();
       if (message.getContent().isEmpty()) return;
       var member = e.getMember().orElse(null);
       if (member == null || member.isBot()) return;
 
-      if(message.getChannelId().equals(config.get("discordbot_channelid"))) {
+      if(message.getChannelId().equals(channel.getId())) {
         String content = message.getContent();
         String user = member.getNickname().get();
         Call.sendMessage(user + content);
