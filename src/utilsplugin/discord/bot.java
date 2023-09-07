16a32012d3d9c4f3f7392316908bb4f9370320db
Hahaa13 <108379326+Hahaa13@users.noreply.github.com>
2023-09-07 07:50:39 +0700

    Update bot.java

--- src/utilsplugin/discord/bot.java
+++ src/utilsplugin/discord/bot.java
@@ -1,1 +1,17 @@
 package utilsplugin.discord;
+
+import utilsplugin.utils.config;
+import discord4j.core.*;
+import discord4j.gateway.intent.*;
+
+public class bot {
+  public static void load() {
+    GatewayDiscordClient gateway = DiscordClient.create(config.get("discordbot_token")
+                                                        .build()
+                                                        .gateway()
+                                                        .setEnabledIntents(IntentSet.of(Intent.GUILD_MEMBERS, Intent.GUILD_MESSAGES))
+                                                        .login()
+                                                        .block();
+    
+  }
+}
